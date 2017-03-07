ed3dc25cf2a1bb615128caa3ffeae257961ce1e1
Raaj108 <rajj108@outlook.com>
2017-03-07 13:55:33 -0800

    modified the view of phoneList

--- _resources/modules/phoneList/phone-list.controller.js
+++ _resources/modules/phoneList/phone-list.controller.js
@@ -1,5 +1,5 @@
 // Define the `PhoneListController` controller on the `phonecatApp` module
-angular.module('phoneList').controller('PhoneListController', ['$scope', '$http', function ($scope, $http) {
+angular.module('phoneList').controller('PhoneListController', ['$scope', '$http', '$log', function ($scope, $http, $log) {
   $http.get('phones/phones.json').then(function (response) {
     $scope.phones = response.data;
   });
--- _resources/modules/phoneList/phone-list.module.js
+++ _resources/modules/phoneList/phone-list.module.js
@@ -1,3 +1,3 @@
 // Define the `phonecatApp` module
-var phoneSpecApp = angular.module('phoneList', []);
+angular.module('phoneList', []);
 
