4e9b04e4c8f5ae1b5d1d0e3f7836c8ee4dbdd435
Rajan <rajj108@gmail.com>
2017-03-11 19:51:56 -0800

    created server to handle the incoming request in the server file, added mimetype dependency

--- phonespecIndex.js
+++ phonespecIndex.js
@@ -1,9 +1,51 @@
 var http = require("http");
-http.createServer(function (request, response) {
-    response.writeHead(200, {
-        "Content-Type": "text/plain"
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
+//handle the sending of 404 error, which usually appears when requested file doesn't exist
+function send404(response) {
+    response.writeHead(404, {
+        "Content-type": "text/plain"
     });
-    response.write("It's alive!");
+    response.write("Error 404: resource not found");
     response.end();
-}).listen(3000);
-console.log("Listening on 3000");
+}
+//sends the contents of the file
+function sendPage(response, filePath, fileContents) {
+    response.writeHead(200, {
+        "Content-type": mime.lookup(path.basename(filePath))
+    });
+    response.end(fileContents);
+}
+//define how our server will handle responses
+function serverWorking(response, absPath) {
+    fs.exists(absPath, function (exists) {
+        if (exists) {
+            fs.readFile(absPath, function (err, data) {
+                if (err) {
+                    send404(response)
+                }
+                else {
+                    sendPage(response, absPath, data);
+                }
+            });
+        }
+        else {
+            send404(response);
+        }
+    });
+}
+//create the HTTP server:
+var server = http.createServer(function (request, response) {
+    var filePath = false;
+    if (request.url == '/') {
+        filePath = "index.html";
+    }
+    else {
+        filePath = "/" + request.url;
+    }
+    var absPath = "./" + filePath;
+    serverWorking(response, absPath);
+});
+var port_number = server.listen(process.env.PORT || 3000);
+console.log("Listening on " + port_number);
