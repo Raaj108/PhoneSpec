6577cd0fba1249985d82fd4c7363bda8427d1b43
Rajan <rajj108@gmail.com>
2017-03-11 18:56:15 -0800

    node's web server file created

---
+++ phonespecIndex.js
@@ -0,0 +1,9 @@
+var http = require("http");
+http.createServer(function (request, response) {
+    response.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    response.write("It's alive!");
+    response.end();
+}).listen(3000);
+console.log("Listening on 3000");
