1dd6ddffa9cbcdfa5746b172203a15edd8bb9564
Raaj108 <rajj108@outlook.com>
2017-04-18 16:16:59 -0700

    called the factory method to get the phone object

--- _resources/modules/phoneDetails/phone-details.controller.js
+++ _resources/modules/phoneDetails/phone-details.controller.js
@@ -1,25 +1,28 @@
 angular.module('phoneDetails').controller('PhoneDetailsController', ['$scope', '$log', '$routeParams', '$http', 'phoneDetailsfactory', function ($scope, $log, $routeParams, $http, phoneDetailsfactory) {
   var phoneName = $routeParams.phone;
   var brandName = $routeParams.brand;
+  $scope.phone = {};
+  /**
+    $http.get('phones/' + $scope.phoneId + '.json').then(function (response) {
 
-  $http.get('phones/' + $scope.phoneId + '.json').then(function (response) {
+      $scope.imageSwap($scope.phone.images[0]);
+    });**/
 
-    $scope.imageSwap($scope.phone.images[0]);
-  });
+
 
   $scope.imageSwap = function (imageURL) {
     $scope.carouselImage = imageURL;
   }
 
-  $scope.getPhone = function (brand, phone) {
+  $scope.getPhone = (function (brand, phone) {
     phoneDetailsfactory.getPhone(brand, phone).
     then(function successCallback(response) {
-      $scope.phone = response.data;
-      console.log($scope.phone)
+      $scope.phone.data = response.data[0];
     }, function errorCallback(response) {
       $log.error(response);
     });
+  })(brandName, phoneName);
+  $log.info($scope.phone);
 
-  }
-  $scope.getPhone(brandName, phoneName);
+
 }]);
